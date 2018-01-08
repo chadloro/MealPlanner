73416ea112b9524144384d6f1da11928d7831421
chadloro <chad.loro.cl@gmail.com>
2018-01-08 11:25:30 -0800

    Clean up register.js

--- js/register.js
+++ js/register.js
@@ -3,34 +3,27 @@ var fieldsValid = false;
 
 function checkPassword() {
     $("#errorspw").empty();
-
+    
     var pass = $("#password").val();
     var confpass = $("#confpword").val();
-    //debugger;
 
     if (pass.length < 8) {
         $("#errorspw").append("Your password must be at least 8 characters.");
     }
-
     validatePassword();
 }
 
 function validatePassword() {
-
     $("#errorsconfpw").empty();
 
     var pass = $("#password").val();
     var confpass = $("#confpword").val();
-
-    console.log("password: " + pass);
-    console.log("confirmation password: " + confpass);
 
     if (confpass.length > 0) {
         if (pass !== confpass) {
             $("#errorsconfpw").append("Your passwords don't match.")
             fieldsValid = false;
         }
-
         else {
             $("#errorsconfpw").append("Passwords match.")
             fieldsValid = true;
